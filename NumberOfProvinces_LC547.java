b7d3b63cb14908c61b50d981d2ef4a46f81e1038
tamannaverma <tamannav01@gmail.com>
2026-09-16 23:54:09 +0530

    graph algo notes

--- NumberOfProvinces_LC547.java
+++ NumberOfProvinces_LC547.java
@@ -2,39 +2,26 @@ class Solution {
     boolean visited[];
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        for(int i=0;i<=n;i++){
-        list.add(new ArrayList<Integer>());
-        }
-    
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    list.get(i+1).add(j+1);
-                    list.get(j+1).add(i+1);
-                    System.out.println((i+1) + " " + (j+1));
-                }
-            }
-        }
-       visited = new boolean[n+1];
+       visited = new boolean[n];
        int count=0;
-        for(int i=1;i<=n;i++){
+        for(int i=0;i<n;i++){
             if(visited[i]==false){
                 count++;
-                dfs(i,-1,list);
+                dfs(i,-1,isConnected);
             }
         }
      return count;
     }
-   public void dfs(int node , int parent , ArrayList<ArrayList<Integer>> list){
+   public void dfs(int node , int parent , int[][] isConnected){
     //System.out.println(node);
-         visited[node]=true; 
-         ArrayList<Integer> neighbours = list.get(node);
-         for(int i=0;i<neighbours.size();i++){
-            int neigh = neighbours.get(i);
-            if(visited[neigh]==false && neigh!=parent){
-                dfs(neigh,node,list);
+       visited[node]=true; 
+       int [] neighbours = isConnected[node];
+         for(int i=0;i<neighbours.length;i++){
+            if(neighbours[i]==1 && visited[i]==false && i!=parent){
+                dfs(i,node,isConnected);
             }
          }
    }
 }
+
+// har row ko adj list smjo of nodes its connected to 
